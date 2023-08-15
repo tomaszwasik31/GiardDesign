a63a65c4972a1d1cfd1a20b3684036ce31d6d434
tomaszwasik31 <tomaszwasik31@gmail.com>
2023-08-15 11:24:39 +0100

    custom screens and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors:{
+        Beige: '#DCC1AB',
+        Black: '#111111',
+        Green: '#1B5B31',
+        Grey: '#F5F0EC'
+
+      }
+    },
   },
   plugins: [],
 }
