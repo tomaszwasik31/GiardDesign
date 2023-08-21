b5752d5aa7ebb04876e8a00188391b7c5f9275e4
tomaszwasik31 <tomaszwasik31@gmail.com>
2023-08-21 15:09:18 +0100

    gallery and cta done, gallery load/lightbox logic

--- src/js/script.js
+++ src/js/script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const toggleButton = document.querySelector(".dropdown-toggle");
   const dropdownMenu = document.querySelector(".dropdown-menu");
 
-  document.addEventListener("click", event => {
+  document.addEventListener("click", (event) => {
     if (toggleButton.contains(event.target)) {
       dropdownMenu.classList.toggle("hidden");
     } else if (!dropdownMenu.contains(event.target)) {
@@ -13,13 +13,12 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-
 // search input
 document.addEventListener("DOMContentLoaded", () => {
   const searchInput = document.querySelector(".search-input");
   const searchButton = document.querySelector(".search-button");
 
-  document.addEventListener("click", event => {
+  document.addEventListener("click", (event) => {
     if (searchButton.contains(event.target)) {
       searchInput.classList.toggle("active");
       if (!searchInput.classList.contains("hidden")) {
@@ -30,4 +29,39 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+//  gallery load script
+
+document.addEventListener("DOMContentLoaded", function () {
+  var imagesToShow = 10;
+  var imagesLoaded = imagesToShow;
+  var imageContainer = document.querySelector("#gallery-container");
+  var loadMoreBtn = document.querySelector("#load-more-btn");
+  var loadMoreMask = document.querySelector("#gallery-load-mask");
+
+  // Initially hide images beyond the first 9
+  var images = imageContainer.querySelectorAll(".img-gallery");
+  for (var i = imagesToShow; i < images.length; i++) {
+    images[i].style.display = "none";
+    images[i].setAttribute("loading", "lazy"); // Add lazy loading attribute
+  }
+
+  // Handle button click to show more images
+  loadMoreBtn.addEventListener("click", function () {
+    for (var i = imagesLoaded; i < imagesLoaded + imagesToShow; i++) {
+      if (images[i]) {
+        images[i].style.display = "block";
+        images[i].setAttribute("loading", "lazy"); // Add lazy loading attribute
+      }
+    }
+    imagesLoaded += imagesToShow;
+
+    // Hide the "Load More" button if all images are shown
+    if (imagesLoaded >= images.length) {
+      loadMoreBtn.style.display = "none";
+      loadMoreMask.style.display = "none";
+    }
+  });
+});
+
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,25 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
+  ],
   theme: {
     screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
     },
     extend: {
-      colors:{
-        beige: '#DCC1AB',
-        black: '#111111',
-        green: '#1B5B31',
-        gray: '#F5F0EC'
-
-      }
+      colors: {
+        beige: "#DCC1AB",
+        black: "#111111",
+        green: "#1B5B31",
+        gray: "#F5F0EC",
+      },
     },
   },
-  plugins: [],
-}
+
+  plugins: [require("tw-elements/dist/plugin.cjs")],
+  darkMode: "class",
+};
